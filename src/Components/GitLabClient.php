e044458b97d9e549611cf156764d70e3686fb2a2
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-01-17 18:00:38 +0200

    GitLab service refactoring

--- src/Components/GitLabClient.php
+++ src/Components/GitLabClient.php
@@ -8,7 +8,6 @@ use GrahamCampbell\GitLab\Authenticators\GitLabAuthenticator;
 use Gitlab\Client;
 use Gitlab\ResultPager;
 use Gitlab\Api\Users;
-use mysql_xdevapi\Result;
 
 class GitLabClient implements ClientInterface
 {
@@ -32,8 +31,10 @@ class GitLabClient implements ClientInterface
         $this->validateConfig($config);
         $client = new Client();
         $auth = new GitLabAuthenticator('http_token');
+
         $this->client = $auth->with($client)->authenticate($config);
         $this->client->setUrl(array_get($config, 'base_url'));
+
         $namespace = array_get($config, 'namespace');
         if (empty($namespace)) {
             $userInfo = $this->client->api('users')->me();
@@ -43,7 +44,6 @@ class GitLabClient implements ClientInterface
             $namespace = $userInfo['username'];
         }
         $this->namespace = $namespace;
-        return $config;
     }
 
     /**
@@ -71,9 +71,10 @@ class GitLabClient implements ClientInterface
         }
     }
 
+    /** {@inheritdoc} */
     public function repoAll($page = 1, $perPage = 50)
     {
-        $listRaw = $this->client->projects()->all([ 'page' => $page , 'per_page' => $perPage ]);
+        $listRaw = $this->client->projects()->all(['page' => $page, 'per_page' => $perPage]);
         $list = [];
         foreach ($listRaw as $item) {
             if (array_get($item, 'namespace.name') === $this->namespace) {
@@ -84,33 +85,27 @@ class GitLabClient implements ClientInterface
         return $list;
     }
 
+    /** {@inheritdoc} */
     public function repoList($repo, $path = null, $ref = null)
     {
-        return $this->client->repositories()->tree($this->getProjectId($repo), [ 'path' => $path, 'ref' => $ref ]);
-//        return $this->client->repo->tree($this->getProjectId($repo), ['path' => $path, 'ref' => $ref]);
+        return $this->client->repositories()->tree($this->getProjectId($repo), ['path' => $path, 'ref' => $ref]);
     }
 
+    /** {@inheritdoc} */
     public function repoGetFileInfo($repo, $path = null, $ref = null)
     {
         $result = $this->repoList($repo, $path, $ref);
-        if(0 === count($result)){
+        if (0 === count($result)) {
             $result = $this->client->repositories()->getFile($this->getProjectId($repo), $path, $ref);
         }
+
         return $result;
-        /*try {
-            return $this->client->repo->getFile($this->getProjectId($repo), $path, $ref);
-        } catch (RuntimeException $e) {
-            if ($e->getCode() === 404) {
-                // File not found possible a directory path. List directory.
-                return $this->repoList($repo, $path, $ref);
-            }
-        }*/
     }
 
+    /** {@inheritdoc} */
     public function repoGetFileContent($repo, $path = null, $ref = null)
     {
-        return $this->client->repositories()->blob($this->getProjectId($repo), $ref, $path);
-//        return $this->client->repositoryFiles()->getRawFile($this->getProjectId($repo), $path, $ref);
+        return $this->client->repositoryFiles()->getRawFile($this->getProjectId($repo), $path, $ref);
     }
 
 }
